da9a4f58623abb60e352aafc8d4b1b59ccfdb031
arjun <arjun.vmaniyara@gmail.com>
2024-01-19 18:32:20 +0530

    first commit

---
+++ js/sendemail.js
@@ -0,0 +1,24 @@
+function sendMail(){
+    var params={
+        name:document.getElementById("name").value ,
+        phone:document.getElementById("phone").value ,
+        email:document.getElementById("email").value ,
+        message:document.getElementById("message").value ,
+    }
+
+const serviceID="service_7urphoj";
+const templateID="template_vjxigd6"
+
+emailjs.send(serviceID,templateID,params).then(res=>{
+    document.getElementById("name").value="";
+    document.getElementById("phone").value ="";
+    document.getElementById("email").value ="";
+    document.getElementById("message").value=""; 
+    console.log(res);
+    alert("your message sent successfully")
+
+
+})
+.catch((err)=>console.log(err));
+}
+
